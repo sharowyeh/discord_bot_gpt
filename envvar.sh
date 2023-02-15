f474da797eed4039b1805f5005ea40c8977d247d
sharow yeh <yuyu.yei@hotmail.com>
2023-02-15 15:35:51 +0800

    update environment variables batch for deployment

---
+++ envvar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# fill up token and api key from discord and openai developer site
+export DISCORD_BOT_TOKEN=
+# default guild id can leave empty if not specify 
+#export DEFAULT_GUILD_ID=
+export OPENAI_API_KEY=
